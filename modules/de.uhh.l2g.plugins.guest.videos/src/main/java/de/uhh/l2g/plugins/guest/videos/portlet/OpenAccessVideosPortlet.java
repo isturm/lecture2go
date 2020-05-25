ef7b0c61a60a26e6ddfde3c582157894376a67f8
Hoffmann, Felix <felix.hoffmann@studium.uni-hamburg.de>
2020-05-25 16:49:23 +0200

    - JWPlayer mit VideoJS ersetzt - Abspielgeschwindigkeit einstellbar - Player-Größe an Webseite angepasst - Vollbildmodus bereits ab Werk vorhanden

--- modules/de.uhh.l2g.plugins.guest.videos/src/main/java/de/uhh/l2g/plugins/guest/videos/portlet/OpenAccessVideosPortlet.java
+++ modules/de.uhh.l2g.plugins.guest.videos/src/main/java/de/uhh/l2g/plugins/guest/videos/portlet/OpenAccessVideosPortlet.java
@@ -27,31 +27,35 @@ import de.uhh.l2g.plugins.util.AutocompleteManager;
  */
 
 @Component(
-	immediate = true,
-	property = {
-	    "com.liferay.portlet.display-category=lecture2go.plugins",
-		"com.liferay.portlet.instanceable=false",
-		"com.liferay.portlet.header-portlet-javascript=/js/jquery.socialshareprivacy.js",
-		"com.liferay.portlet.header-portlet-javascript=/js/qrc.js",
-		"com.liferay.portlet.header-portlet-javascript=/js/jquery.cookie.js",
-		"com.liferay.portlet.header-portlet-javascript=/player/jwplayer-8.4.1/jwplayer.js",
-		"com.liferay.portlet.header-portlet-javascript=/js/mediaCheck-min.js",
-		"com.liferay.portlet.header-portlet-javascript=/js/readmore.min.js",
+		immediate = true,
+		property = {
+				"com.liferay.portlet.display-category=lecture2go.plugins",
+				"com.liferay.portlet.instanceable=false",
+				"com.liferay.portlet.header-portlet-javascript=/js/jquery.socialshareprivacy.js",
+				"com.liferay.portlet.header-portlet-javascript=/js/qrc.js",
+				"com.liferay.portlet.header-portlet-javascript=/js/jquery.cookie.js",
+				"com.liferay.portlet.header-portlet-javascript=/player/video-js-7.8.1/video.min.js",
+				"com.liferay.portlet.header-portlet-javascript=/player/video-js-7.8.1/videojs-contrib-quality-levels.min.js",
+				"com.liferay.portlet.header-portlet-javascript=/player/video-js-7.8.1/videojs-hls-quality-selector.min.js",
+				"com.liferay.portlet.header-portlet-javascript=/player/video-js-7.8.1/videojs-offset.min.js",
+				"com.liferay.portlet.header-portlet-css=/player/video-js-7.8.1/video-js.min.css",
+				"com.liferay.portlet.header-portlet-javascript=/js/mediaCheck-min.js",
+				"com.liferay.portlet.header-portlet-javascript=/js/readmore.min.js",
 //		"com.liferay.portlet.header-portlet-javascript=/js/jquery.dotdotdot.min.js",
-		"com.liferay.portlet.header-portlet-javascript=/js/jquery.mark.js",
-		"com.liferay.portlet.header-portlet-javascript=/js/de.uhh.l2g.plugins.guest.videos.js",
-		"com.liferay.portlet.header-portlet-javascript=/js/jwplayer.custom.util.js",
-		"javax.portlet.display-name=Guest Videos", 
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.name=" + OpenAccessVideosPortletKeys.OPEN_ACCESS_VIDEOS ,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class 
+				"com.liferay.portlet.header-portlet-javascript=/js/jquery.mark.js",
+				"com.liferay.portlet.header-portlet-javascript=/js/de.uhh.l2g.plugins.guest.videos.js",
+				"com.liferay.portlet.header-portlet-javascript=/js/videojs.custom.util.js",
+				"javax.portlet.display-name=Guest Videos",
+				"javax.portlet.init-param.template-path=/",
+				"javax.portlet.name=" + OpenAccessVideosPortletKeys.OPEN_ACCESS_VIDEOS ,
+				"javax.portlet.resource-bundle=content.Language",
+				"javax.portlet.security-role-ref=power-user,user"
+		},
+		service = Portlet.class
 )
 public class OpenAccessVideosPortlet extends MVCPortlet {
 	private static final Log _log = LogFactoryUtil.getLog(OpenAccessVideosPortlet.class);
-	
+
 	@Override
 	public void serveResource( ResourceRequest resourceRequest, ResourceResponse resourceResponse ) throws IOException, PortletException {
 		String resourceID = resourceRequest.getResourceID();
@@ -59,32 +63,32 @@ public class OpenAccessVideosPortlet extends MVCPortlet {
 		if (cmd.equals("get_search_words")) {
 			getSearchWords(resourceRequest, resourceResponse);
 		}
-		
+
 		//--- Autocomplete start
 		//getting task name to do
-	    String task = ParamUtil.getString(resourceRequest, "task");
-	    if (Validator.isNull(task)) {
-	        return;
-	    }
-	    switch (task) {
-	        case"findVideos":
-	        	// get writer for write data
-	        	PrintWriter out = resourceResponse.getWriter();
-	        	JSONArray searchWordsJsonArray = JSONFactoryUtil.createJSONArray();
-	            searchWordsJsonArray = AutocompleteManager.SEARCH_WORDS_JSONArray;
-	            _log.info("Search words array size" + searchWordsJsonArray.length());
-	            out.println(searchWordsJsonArray.toString());
-	            _log.info("End serveResource method");	
-	            break;
-	    }
+		String task = ParamUtil.getString(resourceRequest, "task");
+		if (Validator.isNull(task)) {
+			return;
+		}
+		switch (task) {
+			case"findVideos":
+				// get writer for write data
+				PrintWriter out = resourceResponse.getWriter();
+				JSONArray searchWordsJsonArray = JSONFactoryUtil.createJSONArray();
+				searchWordsJsonArray = AutocompleteManager.SEARCH_WORDS_JSONArray;
+				_log.info("Search words array size" + searchWordsJsonArray.length());
+				out.println(searchWordsJsonArray.toString());
+				_log.info("End serveResource method");
+				break;
+		}
 		//--- Autocomplete end
 	}
-	
+
 	public static JSONArray wordsJSONArray = JSONFactoryUtil.createJSONArray();
-	
+
 	private void getSearchWords(ResourceRequest resourceRequest, ResourceResponse resourceResponse) throws IOException, PortletException {
 		PrintWriter out = resourceResponse.getWriter();
 		out.println(wordsJSONArray);
 	}
-	
+
 }
