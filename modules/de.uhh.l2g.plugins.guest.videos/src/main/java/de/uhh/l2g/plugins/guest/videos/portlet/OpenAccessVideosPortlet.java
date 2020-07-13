841e6844082e0aa8f60acbeee5a0962700f5532e
Hoffmann, Felix <felix.hoffmann@studium.uni-hamburg.de>
2020-07-13 16:04:10 +0200

    Load Video-Sublists on request only

--- modules/de.uhh.l2g.plugins.guest.videos/src/main/java/de/uhh/l2g/plugins/guest/videos/portlet/OpenAccessVideosPortlet.java
+++ modules/de.uhh.l2g.plugins.guest.videos/src/main/java/de/uhh/l2g/plugins/guest/videos/portlet/OpenAccessVideosPortlet.java
@@ -1,12 +1,22 @@
 package de.uhh.l2g.plugins.guest.videos.portlet;
 
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import javax.portlet.Portlet;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
+import com.liferay.portal.kernel.util.ParamUtil;
+import de.uhh.l2g.plugins.model.Video;
+import de.uhh.l2g.plugins.service.VideoLocalServiceUtil;
 import org.osgi.service.component.annotations.Component;
 
 import de.uhh.l2g.plugins.guest.videos.constants.OpenAccessVideosPortletKeys;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author isturm
@@ -19,7 +29,6 @@ import de.uhh.l2g.plugins.guest.videos.constants.OpenAccessVideosPortletKeys;
 		"com.liferay.portlet.header-portlet-javascript=/js/jquery.cookie.js",
 		"com.liferay.portlet.header-portlet-javascript=/js/mediaCheck-min.js",
 		"com.liferay.portlet.header-portlet-javascript=/js/readmore.min.js",
-//		"com.liferay.portlet.header-portlet-javascript=/js/jquery.dotdotdot.min.js",
 		"com.liferay.portlet.header-portlet-javascript=/js/jquery.mark.min.js",
 		"com.liferay.portlet.header-portlet-javascript=/js/de.uhh.l2g.plugins.guest.videos.js",
 		"javax.portlet.display-name=Guest Videos", "javax.portlet.init-param.template-path=/",
@@ -36,5 +45,30 @@ import de.uhh.l2g.plugins.guest.videos.constants.OpenAccessVideosPortletKeys;
 		"com.liferay.portlet.header-portal-css=/o/de.uhh.l2g.plugins-api/player/video-js-plugins/chapter-markers/chapter-markers.css",
 		"com.liferay.portlet.header-portal-css=/o/de.uhh.l2g.plugins-api/player/video-js-7.8.1/video-js.min.css", }, service = Portlet.class)
 public class OpenAccessVideosPortlet extends MVCPortlet {
+	public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+			throws IOException {
+		String resourceID = resourceRequest.getResourceID();
 
+		if (resourceID.equals("getVideosForLectureSeries")) {
+			String objectId = ParamUtil.getString(resourceRequest, "objectId");
+			String videoIds = ParamUtil.getString(resourceRequest, "videoIds");
+
+			List<Video> videos;
+			if (!videoIds.equals("")) {
+				videoIds = videoIds.substring(1, videoIds.length()-1);
+				String[] videoIdsStringArray = videoIds.split(",");
+				long[] videoIdsArray = new long[videoIdsStringArray.length];
+				for (int i=0; i<videoIdsStringArray.length; i++) {
+					videoIdsArray[i] = new Long(videoIdsStringArray[i].trim());
+				}
+				videos = VideoLocalServiceUtil.getByVideoIds(videoIdsArray);
+			} else {
+				videos = VideoLocalServiceUtil.getByLectureseries(new Long(objectId));
+			}
+
+			JSONArray json = JSONFactoryUtil.createJSONArray();
+			json.put(videos);
+			writeJSON(resourceRequest, resourceResponse, json);
+		}
+	}
 }
